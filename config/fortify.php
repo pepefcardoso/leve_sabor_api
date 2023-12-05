937ac4527d96e5be1a4adf10ce7a67cf89cf6a62
pepefcardoso <pppfcardoso@gmail.com>
2023-12-05 10:23:08 -0300

    feature: fortify 2-factor auth configuration

--- config/fortify.php
+++ config/fortify.php
@@ -148,13 +148,12 @@ return [
         Features::registration(),
         Features::resetPasswords(),
         Features::emailVerification(),
-//        Features::updateProfileInformation(),
-//        Features::updatePasswords(),
-//        Features::twoFactorAuthentication([
+        Features::updateProfileInformation(),
+        Features::updatePasswords(),
+        Features::twoFactorAuthentication([
 //            'confirm' => true,
-//            'confirmPassword' => true,
-//            // 'window' => 0,
-//        ]),
+            'confirmPassword' => true,
+        ]),
     ],
 
 ];
