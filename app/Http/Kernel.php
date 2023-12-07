e88635340222fc1b68e857e499529fc1c737f24b
pepefcardoso <pppfcardoso@gmail.com>
2023-12-07 13:18:06 -0300

    fix:fixing routing errors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
+// use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            EnsureEmailIsVerified::class,
+            // EnsureEmailIsVerified::class,
         ],
     ];
 
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // 'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
